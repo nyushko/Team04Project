cdce4d7239f5d6319fc729cc26d07aad957c7d8a
gramirezualberta <gramirez@ualberta.ca>
2014-02-25 19:20:31 -0700

    fixing errors

--- src/com/example/team04project/Reply.java
+++ src/com/example/team04project/Reply.java
@@ -2,9 +2,15 @@ package com.example.team04project;
 
 
 
-public class Reply extends 
-Comments
+public class Reply extends Comments
 {
+	public Reply(String comment, String date, String user)
+	{
+
+		super(comment, date, user);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void addReply(){
 		
 	}
--- src/com/example/team04project/TopLevel.java
+++ src/com/example/team04project/TopLevel.java
@@ -2,9 +2,16 @@ package com.example.team04project;
 
 
 
-public class TopLevel extends 
-Comments
+public class TopLevel extends Comments
 {
+
+	public TopLevel(String comment, String date, String user)
+	{
+
+		super(comment, date, user);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void addTopLevel(){
 		
 	}
